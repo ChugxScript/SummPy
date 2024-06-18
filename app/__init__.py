9f96dc84c8921ac2603ab94606f588edaf467fdd
ChugxScript <arw.oloroso@gmail.com>
2024-06-18 17:36:55 +0800

    [done] file structure

---
+++ app/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from .blueprints.get_started import get_started
+# from .blueprints.summary import summary
+from .blueprints.about import about
+from .blueprints.home import home
+
+def create_app():
+    app = Flask(__name__)
+    app.secret_key = 'secret-key'
+    app.register_blueprint(home)
+    app.register_blueprint(about)
+    app.register_blueprint(get_started)
+    # app.register_blueprint(summary)
+
+    return app
