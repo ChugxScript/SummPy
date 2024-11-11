9575723a9bbb3fad01aae557a1c1d744d5ab7e38
ChugxScript <arw.oloroso@gmail.com>
2024-11-12 04:15:34 +0800

    update admin add edit

--- app/blueprints/admin/admin_dashboard.py
+++ app/blueprints/admin/admin_dashboard.py
@@ -4,4 +4,4 @@ admin_dashboard = Blueprint('admin_dashboard', __name__)
 
 @admin_dashboard.route("/admin_dashboard")
 def admin_dashboard_page():
-    return render_template('admin/admin_dashboard.html')
+    return render_template('admin/COS Library - Admin (dashboard).html')
